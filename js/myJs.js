a950f6dce235aadfc7f2b1a1bd5d22868c1c5eb9
hoanganhluong0512 <88782784+hoanganhluong0512@users.noreply.github.com>
2021-08-12 04:03:03 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -13,6 +13,7 @@ const textConfig = {
     "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
   text12: "Okii lunn <3",
 };
+
 $(document).ready(function () {
   // process bar
   setTimeout(function () {

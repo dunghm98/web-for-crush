e319d2ce2492930408b6f145728dbce1966b1f2b
hoanganhluong0512 <88782784+hoanganhluong0512@users.noreply.github.com>
2021-08-11 23:42:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/hoanganhluong1010
+    messLink: 'https://www.facebook.com/hoanganhluong1010/' //link mess của các bạn. VD: https://m.me/hoanganhluong1010

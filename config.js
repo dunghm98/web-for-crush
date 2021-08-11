03a50127f2463703218fc6fb0f0b84c7f8d394f9
hoanganhluong0512 <88782784+hoanganhluong0512@users.noreply.github.com>
2021-08-11 23:40:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,4 +16,4 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/hoanganhluong1010

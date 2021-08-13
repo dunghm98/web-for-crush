9b25eb9492f3b32200deb6ced92da7fb33a97f78
Nguyễn Hải Anh <88780632+nguyenhaianh2003@users.noreply.github.com>
2021-08-13 13:23:23 +0700

    ils.js
    nguyenhaianh2003
    /
    web-cho-crush

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text1: "He luu em!",
+  text2: "anh có điều này muốn hỏi em nhớ phải trả lời thật lòng nhaaa.",
+  text3: "em yêu anh có phải không nào ._.",
+  text4: "Nếu em ko trả lời mà thoát ra tức là anh biết đó , bởi vì anh có cài hệ thống đó  :v",
+  text5: "anh mơ à???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text7: "lí do em thích anh đi :vvvv",
+  text8: "Gửi cho anh <3",
+  text9: "Vì anh đẹp trai vlllll, với cả em yêu anh nhiều ",
+  text10: "anh biết mà !^^ Anh Yêu Em <3",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    " Xong gòi đó !!! , cảm ơn em đã trả lời câu hỏi của anh !! Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
   text12: "Okii lunn <3",
 };
 

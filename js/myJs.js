1a879fe0294d42e810defa3c0ffffe49cf631cdb
anhphuoq <91332301+anhphuoq@users.noreply.github.com>
2021-09-24 21:27:00 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
+  text1: "Chào Thư!",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
+  text3: "Bi chinh nắm đún khong ._.",
+  text4: "Nếu cậu ko trả lời mà thoát ra là t đạp chetme chừ nha :v",
+  text5: "Khong xấu quéc",
+  text6: "Chinh nắm  <3",
+  text7: "Tại sao lại thấy bii đẹp :vvvv",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
+  text9: "Vì Bi chinh gáu vlllll",
   text10: "Tớ biết mà ^^ Yêu cậu 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Tối nay mìn cua phim nha",
   text12: "Okii lunn <3",
 };
 

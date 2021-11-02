35a572d0ce49115ebc4b5d5aac1b364f2698acaf
chichichi2203 <93566378+chichichi2203@users.noreply.github.com>
2021-11-02 11:42:18 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "Hello anh!",
+  text2: "Anh ơi.",
+  text3: "DOVE-Do yOu loVe mE? ._.",
+  text4: "Chàng học viên từng là của cô ấy. Giờ đây là chàng sĩ quan của em :v",
+  text5: "Mơ à???",
+  text6: "Rất yêu em <3",
+  text7: "Thế giới 7 tỉ người tại sao anh lại yêu em nhỉ :vvvv",
+  text8: "Gửi cho em <3",
+  text9: "Em xinh này, đáng yêu này, ngoan này....",
+  text10: "Em biết mà yêu anh",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Cứ yêu em như này mãi nhé",
+  text12: "Yêu em <3",
 };
 
 $(document).ready(function () {

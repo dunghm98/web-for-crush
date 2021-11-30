fdd74136c25cc0debc15fe2af4e50ee67cf81e53
hanyla <95296712+hanyla@users.noreply.github.com>
2021-11-30 21:48:30 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,13 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
-  text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+  text1: "trào cậu",
+  text2: "bây giờ tớ hỏi kậu trả lờy",
+  text3: "hải đẹp gái",
+  text5: "đồ sấu cí",
+  text6: "hải sieu cấp đẹp gái",
+  text7: "đẹp gái thật hã",
+  text8: "send",
+  text9: "siêu đẹp gái",
+  text10: "i know",
 };
 
 $(document).ready(function () {
